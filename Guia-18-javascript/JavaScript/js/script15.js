10200eec05ee8138be6a0285775bd24bd156224d
Facu almará <facundoalmara30@gmail.com>
2023-09-08 11:49:32 -0300

    8/9

---
+++ Guia-18-javascript/JavaScript/js/script15.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document
+        .getElementById("estadoForm")
+        .addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            const radio = document.getElementById(`radio`).value;
+            const area = calcularAreaCirculo(radio);
+            const perimetro = calcularPerimetroCirculo(radio);
+
+            console.log(`area del ciculo:${area.toFixed(2)}`);
+            console.log(`perimetro del ciruclo:${perimetro.toFixed(2)}`);
+            resultado.innerHTML = `
+        <table class="table table-primary">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Radio</th>
+                        <th scope="col">Area</th>
+                        <th scope="col">Perimetro</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td> ${radio}</td>
+                        <td> ${area.toFixed(2)}</td>
+                        <td> ${perimetro.toFixed(2)}</td>
+                    </tr>
+        `;
+
+            // Función para calcular el área de un círculo
+            function calcularAreaCirculo(radio) {
+                return Math.PI * radio * radio;
+            }
+
+            // Función para calcular el perímetro de un círculo
+            function calcularPerimetroCirculo(radio) {
+                return 2 * Math.PI * radio;
+            }
+        });
+});
---
+++ Guia-18-javascript/JavaScript/script21.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let arrayPersonas = [];
+
+    document
+        .getElementById("estadoForm")
+        .addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            const nombre = document.getElementById("nombre").value;
+            const edad = document.getElementById("edad").value;
+            const sexos = document.getElementById("sexo").value;
+            const peso = document.getElementById("peso").value;
+            const altura = document.getElementById("altura").value;
+
+            console.log(nombre);
+            console.log(edad);
+            console.log(sexos);
+            console.log(peso);
+            console.log(altura);
+            let sexo = "";
+            if (sexo == 1) {
+                sexo = "Hombre";
+            } else if (sexo == 2) {
+                sexo = " Mujer";
+            } else {
+                sexo = "Otro";
+            }
+
+            let persona = new Object();
+            persona.nombre = nombre;
+            persona.edad = edad;
+            persona.sexo = sexo;
+            persona.peso = peso;
+            persona.altura = altura;
+
+            arrayPersonas.push(persona);
+
+            console.log(persona);
+            console.log(arrayPersonas);
+
+            let resultado = `
+    <br>nombre: ${nombre}
+    <br>edad: ${edad}
+    <br>sexo: ${sexo}
+    <br>peso: ${peso}
+    <br>altura: ${altura}
+    `;
+    
+// Obtener el elemento donde deseas agregar el contenido
+let resultadoElement = document.getElementById('resultado');
+// Agregar el contenido HTML al elemento utilizando innerHTML
+resultadoElement.innerHTML = resultadoHTML;
+        });
+});
